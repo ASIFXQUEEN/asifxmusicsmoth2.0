7b403739a2b16aa50b5eda725c99e323f726c1c0
RAJ RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2025-03-05 19:51:11 +0530

    Update play.py

--- SYSTUM/utils/inline/play.py
+++ SYSTUM/utils/inline/play.py
@@ -51,7 +51,7 @@ def stream_markup_timer(_, chat_id, played, dur):
     elif 80 <= umm < 95:
         bar = "─────────♪─"   
     else:
-        bar = "───ᴋʜᴀᴛᴀᴍ────"
+        bar = "───ᴋʜᴀᴛᴀᴍ───"
         
     buttons = [
         [
