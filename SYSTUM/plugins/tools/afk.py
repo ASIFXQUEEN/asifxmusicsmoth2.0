709590e89ac579d8bc53ce65ac69ae658b42d84a
RAJ RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2025-03-24 13:33:34 +0530

    Update afk.py

--- SYSTUM/plugins/tools/afk.py
+++ SYSTUM/plugins/tools/afk.py
@@ -4,9 +4,6 @@ from pyrogram.enums import MessageEntityType
 from pyrogram import filters
 from pyrogram.types import Message
 from SYSTUM import app
-from SYSTUM.mongo.readable_time import get_readable_time
-from SYSTUM.mongo.afkdb import add_afk, is_afk, remove_afk
-
 
 
 @app.on_message(filters.command(["afk", "brb"], prefixes=["/", "!"]))
